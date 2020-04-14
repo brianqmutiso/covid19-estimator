935fc6e1c6d0b61f1d429a3cb115e8c6627b4f8b
francis warui <franciskinyuru26@gmail.com>
2020-04-14 16:03:54 +0000

    checks

--- src/estimator.php
+++ src/estimator.php
@@ -13,35 +13,7 @@ if($sr->periodType=="days"){
 $data=array("data"=>$sr);
 $estimate=array("estimate"=>array_merge(impact($sr,$timeToElapse),severeImpact($sr,$timeToElapse)));
 $all_array=array_merge($data,impact($sr,$timeToElapse),severeImpact($sr,$timeToElapse));
-//$all=json_encode($all_array,JSON_FORCE_OBJECT);
-/*if (isset($request->datas)) {
-  if ($request->datas=="json") {
-    logs();
-    return json_decode($all,true);
-  }
-  if ($request->datas=="logs") {
-   if (file_exists("logs.json")) {
-   $logfile="logs.json";
-$lof=file_get_contents("logs.json");
-$all_val=json_decode("[".$lof."]",true);
-foreach ($all_val as $key => $value) {
-print_r($value['current_timestamp']."\t\t".$value['URL']."\t\t".$value['response']."\t\t".$value['time']."\n");} } 
-    else{
-      return "no logs";
-    } }
-if ($request->datas=="xml") {
-$dat=json_encode($data);
-$data=json_decode($dat,true);
-$array = array ('xmldata' => 'xmldata',$data,impact($sr),severeImpact($sr),
-);
-$xml_data =xml_data($array, false );
-logs();
-return $xml_data->asXML();
-}}
-else{
- logs();
-return json_decode($all,true);
-}*/
+
 print_r($all_array);
 return $all_array;
 }
@@ -73,35 +45,5 @@ return array("impact"=>$impact);
     $severeImpact=array("currentlyInfected"=>$currentInfected,"infectionsByRequestedTime"=>$infectionsByRequestedTime,"severeCasesByRequestedTime"=>$severeCasesByRequestedTime,"hospitalBedsByRequestedTime"=>$hospitalBedsByRequestedTime,"casesForICUByRequestedTime"=>$casesForICUByRequestedTime,"casesForVentilatorsByRequestedTime"=>$casesForVentilatorsByRequestedTime,"dollarsInFlight"=>round($dollarsInFlight,2));
 return array("severeImpact"=>$severeImpact);
   }
-function xml_data( $data, $xml=false) {
-   if (!$xml) {$xml = new \SimpleXMLElement("<root></root>"); }
-    foreach($data as $key => $value) {
-    if(is_array($value)) {
-      if(!is_numeric($key)){
-        $subnode = $xml->addChild($key);
-        foreach ($value as $key => $value1) {
-if (!is_array($value1)) {
-$subnode->addChild("$key","$value1");}
-else{$subnode1= $subnode->addChild($key);
-   foreach ($value1 as $key => $value2) {
-   $subnode1->addChild("$key","$value2");
-   }} }  }
-      else{
-        xml_data($value, $xml); }}
-    else {$xml->addChild("$key","$value");
-    }}
-  return $xml;
-}
-function logs(){
-  $posts=array("current_timestamp"=>time(),"URL"=>"","response"=>http_response_code(),"time"=>"");
-if (!file_exists("logs.json")) {
- $logfile="logs.json";
-$lof=fopen($logfile, 'w');
-fwrite($lof, json_encode($posts));
-fclose($lof);
-}else{
-  $logfile="logs.json";
-$lof=fopen($logfile, 'a');
-fwrite($lof,",".json_encode($posts));
-fclose($lof);}}
+
 
